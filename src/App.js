1bfd6299a24395222c79c54aeb6359b9dd3a180b
Sammwy <sammwy.dev@gmail.com>
2022-11-18 07:23:16 -0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import Query from "./graphql/query";
-import { FindProfileQuery } from "./graphql/queries";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import E404 from "./pages/_404";
+
+import Home from "./pages/home";
+import Landing from "./pages/landing";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+import Register from "./pages/register";
+import Settings from "./pages/settings";
+import Verify from "./pages/verify";
 
 function App() {
   return (
-    <div>
-      <Query
-        query={FindProfileQuery}
-        variables={{ username: "sammwy" }}
-        render={({ error, props }) => {
-          return <div> {props && JSON.stringify(props)} </div>;
-        }}
-      />
-    </div>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/verify" element={<Verify />} />
+          <Route path="/:username" element={<Profile />} />
+
+          <Route path="*" element={<E404 />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
